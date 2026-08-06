e21f61c81bda4620c42947c006db1c0762b45b09
Michael_Orden <110533700+michaelorden@users.noreply.github.com>
2026-08-06 22:09:11 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   admin_username = "azureuser"
 
   disable_password_authentication = false
-  admin_password                  = "var.admin_password"
+  admin_password                  = var.admin_password
 
   network_interface_ids = [
     azurerm_network_interface.nic.id
