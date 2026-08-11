5ed8a1f3dbd763a952fd1bf872359dec897a394c
Michael_Orden <110533700+michaelorden@users.noreply.github.com>
2026-08-11 14:32:51 +1200

    Remove second and third VM configurations
    Removed configuration for multiple VMs and associated resources.

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = "*"
+    source_address_prefix     = "*"
     destination_address_prefix = "*"
   }
 
@@ -46,7 +46,7 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "80"
-    source_address_prefix      = "*"
+    source_address_prefix     = "*"
     destination_address_prefix = "*"
   }
 }
@@ -94,146 +94,6 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   network_interface_ids = [
     azurerm_network_interface.nic.id
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "ubuntu-24_04-lts"
-    sku       = "server"
-    version   = "latest"
-  }
-}
-
-# ----------------------------------------
-# Second VM Public IP
-# ----------------------------------------
-
-resource "azurerm_public_ip" "pip2" {
-  name                = "lab-public-ip-2"
-  location            = azurerm_resource_group.lab.location
-  resource_group_name = azurerm_resource_group.lab.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-# ----------------------------------------
-# Second VM Network Interface
-# ----------------------------------------
-
-resource "azurerm_network_interface" "nic2" {
-  name                = "lab-nic-2"
-  location            = azurerm_resource_group.lab.location
-  resource_group_name = azurerm_resource_group.lab.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip2.id
-  }
-}
-
-# ----------------------------------------
-# Associate NSG with Second NIC
-# ----------------------------------------
-
-resource "azurerm_network_interface_security_group_association" "assoc2" {
-  network_interface_id      = azurerm_network_interface.nic2.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-# ----------------------------------------
-# Second Ubuntu VM
-# ----------------------------------------
-
-resource "azurerm_linux_virtual_machine" "vm2" {
-  name                = "ubuntu-vm-2"
-  resource_group_name = azurerm_resource_group.lab.name
-  location            = azurerm_resource_group.lab.location
-  size                = "Standard_B2s_v2"
-
-  admin_username = "azureuser"
-
-  disable_password_authentication = false
-  admin_password                  = var.admin_password
-
-  network_interface_ids = [
-    azurerm_network_interface.nic2.id
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "ubuntu-24_04-lts"
-    sku       = "server"
-    version   = "latest"
-  }
-}
-
-# ----------------------------------------
-# Third VM Public IP
-# ----------------------------------------
-
-resource "azurerm_public_ip" "pip3" {
-  name                = "lab-public-ip-3"
-  location            = azurerm_resource_group.lab.location
-  resource_group_name = azurerm_resource_group.lab.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-# ----------------------------------------
-# Third VM Network Interface
-# ----------------------------------------
-
-resource "azurerm_network_interface" "nic3" {
-  name                = "lab-nic-3"
-  location            = azurerm_resource_group.lab.location
-  resource_group_name = azurerm_resource_group.lab.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip3.id
-  }
-}
-
-# ----------------------------------------
-# Associate NSG with Third NIC
-# ----------------------------------------
-
-resource "azurerm_network_interface_security_group_association" "assoc3" {
-  network_interface_id      = azurerm_network_interface.nic3.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-# ----------------------------------------
-# Third Ubuntu VM
-# ----------------------------------------
-
-resource "azurerm_linux_virtual_machine" "vm3" {
-  name                = "ubuntu-vm-3"
-  resource_group_name = azurerm_resource_group.lab.name
-  location            = azurerm_resource_group.lab.location
-  size                = "Standard_B2s_v2"
-
-  admin_username = "azureuser"
-
-  disable_password_authentication = false
-  admin_password                  = var.admin_password
-
-  network_interface_ids = [
-    azurerm_network_interface.nic3.id
   ]
 
   os_disk {
